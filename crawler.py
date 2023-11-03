a34b4323ab0f3177c4e0b1fbdfa78c9b7d19a2e0
Johnson <voth.jojo@gmx.de>
2023-11-03 12:56:19 +0100

    basic link fetching

--- crawler.py
+++ crawler.py
@@ -6,4 +6,10 @@ r = requests.get('https://www.uos.de')
 
 # parse html into usable object
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
+
+# find all links
+links = soup.find_all('a')
+
+# print all links
+for link in links:
+    print(link.get('href'))
