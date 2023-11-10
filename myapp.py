e1b0c6ab000ec83751a70abd2f8a12c0917a0959
Johnson <voth.jojo@gmx.de>
2023-11-10 13:11:37 +0100

    basic rev stuff from lecture

---
+++ myapp.py
@@ -0,0 +1,19 @@
+# myapp.py
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+# Define a route for your main page
+def index():
+    return render_template('index.html')
+
+
+@app.route('/reverse')
+def reverse():
+    # grab the GET request arguments
+    rev = request.args['rev']
+
+    #  return the reverse.html and pass the rev argument
+    return render_template('reverse.html', rev=rev[::-1])
